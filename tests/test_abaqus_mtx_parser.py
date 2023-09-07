b7198e28a292cef1c3f0d8069f68de64da952e59
vincint-skywalker <huang-lihao@outlook.com>
2023-09-07 19:21:25 +0800

    new test

--- tests/test_abaqus_mtx_parser.py
+++ tests/test_abaqus_mtx_parser.py
@@ -12,7 +12,7 @@ from abaqus_mtx_parser import parse_mtx
 class TestAbaqusMtxParser(unittest.TestCase):
 
     def test_unsymmetric_stiffness(self):
-        mtx = files("abaqus_mtx_parser.mtx").joinpath("unsym.mtx")
+        mtx = files("abaqus_mtx_parser.mtx.unsymmetric").joinpath("inner.mtx")
         result = parse_mtx(mtx)
 
         # Test nodes and dof
@@ -29,9 +29,9 @@ class TestAbaqusMtxParser(unittest.TestCase):
                 v_pair[0], v_pair[1], delta = v_pair[1] * 1e-5)
 
     def test_symmetric_stiffness(self):
-        mtx = files("abaqus_mtx_parser.mtx").joinpath("sym.mtx")
+        mtx = files("abaqus_mtx_parser.mtx.symmetric").joinpath("inner.mtx")
         result = parse_mtx(mtx)
-        print(result.data)
+
         # Test nodes and dof
         self.assertListEqual(result.nodes, [2, 3, 4, 5, 6, 7])
         self.assertListEqual(result.dof, [1, 2, 3, 4, 5, 6])
@@ -44,7 +44,52 @@ class TestAbaqusMtxParser(unittest.TestCase):
         ]:
             self.assertAlmostEqual(
                 v_pair[0], v_pair[1], delta = v_pair[1] * 1e-5)
+    
+    def test_symmetric_static_condensation(self):
+        inner = parse_mtx(
+            files("abaqus_mtx_parser.mtx.symmetric").joinpath("inner.mtx"))
+        outer = parse_mtx(
+            files("abaqus_mtx_parser.mtx.symmetric").joinpath("outer.mtx"))
+        total = parse_mtx(
+            files("abaqus_mtx_parser.mtx.symmetric").joinpath("total.mtx"))
 
+        K = np.zeros((72, 72))
+        E = np.arange( 0, 36,  1)
+        R = np.arange(36, 72,  1)
+        K[np.ix_(E, E)] += inner.stiffness
+        K += outer.stiffness
+
+        K_EE = K[np.ix_(E, E)]
+        K_RE = K[np.ix_(R, E)]
+        K_ER = K[np.ix_(E, R)]
+        K_RR = K[np.ix_(R, R)]
+
+        K1 = K_RR - np.linalg.multi_dot([K_RE, np.linalg.inv(K_EE), K_ER])
+
+        self.assertTrue(np.allclose(K1, total.stiffness))
+    
+    def test_unsymmetric_static_condensation(self):
+        inner = parse_mtx(
+            files("abaqus_mtx_parser.mtx.unsymmetric").joinpath("inner.mtx"))
+        outer = parse_mtx(
+            files("abaqus_mtx_parser.mtx.unsymmetric").joinpath("outer.mtx"))
+        total = parse_mtx(
+            files("abaqus_mtx_parser.mtx.unsymmetric").joinpath("total.mtx"))
+
+        K = np.zeros((72, 72))
+        E = np.arange( 0, 36,  1)
+        R = np.arange(36, 72,  1)
+        K[np.ix_(E, E)] += inner.stiffness
+        K += outer.stiffness
+
+        K_EE = K[np.ix_(E, E)]
+        K_RE = K[np.ix_(R, E)]
+        K_ER = K[np.ix_(E, R)]
+        K_RR = K[np.ix_(R, R)]
+
+        K1 = K_RR - np.linalg.multi_dot([K_RE, np.linalg.inv(K_EE), K_ER])
+
+        self.assertTrue(np.allclose(K1, total.stiffness))
 
 if __name__ == "__main__":
     unittest.main()
